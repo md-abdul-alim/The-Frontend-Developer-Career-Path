217121a849220b18721063fca5adff9699a7dda1
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-03-22 17:44:57 +0600

    diary

---
+++ Module 3. Making websites interactive/7.Build a diary app/index.js
@@ -0,0 +1,28 @@
+const entryFrom = document.getElementById("entryForm")
+const entriesSection = document.querySelector("#entries")
+const entryTextBox = document.querySelector(".entry-textbox")
+const entriesNav = document.querySelector(".entries-nav")
+let count = 1;
+function addEntryToDom(event){
+    event.preventDefault();
+    const entryDiv = document.createElement("div")
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextBox.value;
+    entryDiv.style.display = 'none';
+    entriesSection.appendChild(entryDiv);
+    entryTextBox.value = '';
+
+    const displayEntryButton = document.createElement('button')
+    displayEntryButton.className = 'display-entry-button'
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton)
+    displayEntryButton.addEventListener("click", function(){
+        const allEntries = document.querySelectorAll('.single-entry');
+        for(let index = 0; index < allEntries.length; index++){
+            allEntries[index].style.display = 'none';
+        }
+        entryDiv.style.display = 'block';
+    });
+    count++;
+}
+entryFrom.addEventListener("submit", addEntryToDom)
